77efb6e4c5f9a924533bdfa669eb37d5cbef42b2
James Tauber <jtauber@jtauber.com>
2008-05-16 17:24:53 +0000

    form for requesting friendship
    git-svn-id: http://django-friends.googlecode.com/svn/trunk@9 f9456ae0-1038-0410-abc2-2f1ed958ef3d

---
+++ friendsdev/friends/forms.py
@@ -0,0 +1,42 @@
+from django import newforms as forms
+
+from django.contrib.auth.models import User
+
+from friends.models import FriendshipInvitation
+
+class UserForm(forms.Form):
+    
+    def __init__(self, user=None, *args, **kwargs):
+        self.user = user
+        super(UserForm, self).__init__(*args, **kwargs)
+
+
+class InviteFriendForm(UserForm):
+    
+    to_user = forms.CharField(widget=forms.HiddenInput)
+    message = forms.CharField(label="Message", required=False, widget=forms.Textarea(attrs = {'cols': '20', 'rows': '5'}))
+    
+    def clean_to_user(self):
+        to_username = self.cleaned_data["to_user"]
+        try:
+            User.objects.get(username=to_username)
+        except User.DoesNotExist:
+            raise forms.ValidationError(u"Unknown user.")
+            
+        return self.cleaned_data["to_user"]
+    
+    def clean(self):
+        to_user = User.objects.get(username=self.cleaned_data["to_user"])
+        previous_invitations = FriendshipInvitation.objects.filter(to_user=to_user, from_user=self.user)
+        if previous_invitations.count() > 0:
+            raise forms.ValidationError(u"Already requested friendship with %s" % to_user.username)
+        return self.cleaned_data
+    
+    def save(self):
+        to_user = User.objects.get(username=self.cleaned_data["to_user"])
+        message = self.cleaned_data["message"]
+        invitation = FriendshipInvitation(from_user=self.user, to_user=to_user, message=message, status=2)
+        # @@@ no django-notification yet
+        invitation.save()
+        self.user.message_set.create(message="Friendship requested with %s" % to_user.username)
+        return invitation
