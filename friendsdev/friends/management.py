cf1c0f01164d9e98076f4180a16c0e37af4c78a1
James Tauber <jtauber@jtauber.com>
2008-05-22 10:45:22 +0000

    update for changes in django-notification r9
    git-svn-id: http://django-friends.googlecode.com/svn/trunk@14 f9456ae0-1038-0410-abc2-2f1ed958ef3d

--- friendsdev/friends/management.py
+++ friendsdev/friends/management.py
@@ -5,8 +5,8 @@ try:
     from notification import models as notification
     
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("friends_invite")
-        notification.create_notice_type("friends_accept")
+        notification.create_notice_type("friends_invite", "Invitation", "you have received an invitation")
+        notification.create_notice_type("friends_accept", "Acceptance", "an invitation you sent has been accepted")
     
     dispatcher.connect(create_notice_types, signal=signals.post_syncdb, sender=notification)
 except ImportError:
