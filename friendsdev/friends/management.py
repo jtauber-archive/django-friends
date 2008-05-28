8617e7ab114a347d38caf4ee951134491b4ac2e2
James Tauber <jtauber@jtauber.com>
2008-05-28 22:26:49 +0000

    added default
    git-svn-id: http://django-friends.googlecode.com/svn/trunk@26 f9456ae0-1038-0410-abc2-2f1ed958ef3d

--- friendsdev/friends/management.py
+++ friendsdev/friends/management.py
@@ -5,12 +5,12 @@ try:
     from notification import models as notification
     
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("friends_invite", "Invitation Received", "you have received an invitation")
-        notification.create_notice_type("friends_invite_sent", "Invitation Sent", "you have sent an invitation")
-        notification.create_notice_type("friends_accept", "Acceptance Received", "an invitation you sent has been accepted")
-        notification.create_notice_type("friends_accept_sent", "Acceptance Sent", "you have accepted an invitation you received")
-        notification.create_notice_type("friends_otherconnect", "Other Connection", "one of your friends has a new connection")
-        notification.create_notice_type("join_accept", "Join Invitation Accepted", "an invitation you sent to join this site has been accepted")
+        notification.create_notice_type("friends_invite", "Invitation Received", "you have received an invitation", default=2)
+        notification.create_notice_type("friends_invite_sent", "Invitation Sent", "you have sent an invitation", default=1)
+        notification.create_notice_type("friends_accept", "Acceptance Received", "an invitation you sent has been accepted", default=2)
+        notification.create_notice_type("friends_accept_sent", "Acceptance Sent", "you have accepted an invitation you received", default=1)
+        notification.create_notice_type("friends_otherconnect", "Other Connection", "one of your friends has a new connection", default=2)
+        notification.create_notice_type("join_accept", "Join Invitation Accepted", "an invitation you sent to join this site has been accepted", default=2)
     
     dispatcher.connect(create_notice_types, signal=signals.post_syncdb, sender=notification)
 except ImportError:
