33682756a3def708f0a1fd0435894c31b3131fed
James Tauber <jtauber@jtauber.com>
2008-06-06 00:41:50 +0000

    added yahoo address book importer
    git-svn-id: http://django-friends.googlecode.com/svn/trunk@28 f9456ae0-1038-0410-abc2-2f1ed958ef3d

--- friendsdev/friends/importer.py
+++ friendsdev/friends/importer.py
@@ -1,6 +1,10 @@
+from django.conf import settings
+
 from models import Contact
 
 import vobject
+import ybrowserauth
+import simplejson
 
 def import_vcards(stream, user):
     """
@@ -23,4 +27,32 @@ def import_vcards(stream, user):
                 imported += 1
         except AttributeError:
             pass # missing value so don't add anything
+    return imported, total
+
+def import_yahoo(bbauth_token, user):
+    """
+    Uses the given BBAuth token to retrieve a Yahoo Address Book and
+    import the entries with an email address into the contacts of the
+    given user.
+    
+    Returns a tuple of (number imported, total number of entries).
+    """
+    
+    ybbauth = ybrowserauth.YBrowserAuth(settings.BBAUTH_APP_ID, settings.BBAUTH_SHARED_SECRET)
+    ybbauth.token = bbauth_token
+    address_book_json = ybbauth.makeAuthWSgetCall("http://address.yahooapis.com/v1/searchContacts?format=json&email.present=1&fields=name,email")
+    address_book = simplejson.loads(address_book_json)
+    
+    total = 0
+    imported = 0
+    
+    for contact in address_book["contacts"]:
+        total += 1
+        email = contact['fields'][0]['data']
+        name = contact['fields'][1]['first'] + contact['fields'][1]['last']
+        try:
+            Contact.objects.get(user=user, email=email)
+        except Contact.DoesNotExist:
+            Contact(user=user, name=name, email=email).save()
+            imported += 1
     return imported, total
