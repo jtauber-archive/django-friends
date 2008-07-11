d6319e8336b5728361ea6650b4f4c447612cd682
Jannis Leidel <jannis@leidel.info>
2008-07-11 09:26:56 +0000

    marked notification descriptions as translateable
    git-svn-id: http://django-friends.googlecode.com/svn/trunk@37 f9456ae0-1038-0410-abc2-2f1ed958ef3d

--- friendsdev/friends/management.py
+++ friendsdev/friends/management.py
@@ -1,16 +1,18 @@
 from django.dispatch import dispatcher
 from django.db.models import signals
 
+from django.utils.translation import ugettext_noop as _
+
 try:
     from notification import models as notification
     
     def create_notice_types(app, created_models, verbosity, **kwargs):
-        notification.create_notice_type("friends_invite", "Invitation Received", "you have received an invitation", default=2)
-        notification.create_notice_type("friends_invite_sent", "Invitation Sent", "you have sent an invitation", default=1)
-        notification.create_notice_type("friends_accept", "Acceptance Received", "an invitation you sent has been accepted", default=2)
-        notification.create_notice_type("friends_accept_sent", "Acceptance Sent", "you have accepted an invitation you received", default=1)
-        notification.create_notice_type("friends_otherconnect", "Other Connection", "one of your friends has a new connection", default=2)
-        notification.create_notice_type("join_accept", "Join Invitation Accepted", "an invitation you sent to join this site has been accepted", default=2)
+        notification.create_notice_type("friends_invite", _("Invitation Received"), _("you have received an invitation"), default=2)
+        notification.create_notice_type("friends_invite_sent", _("Invitation Sent"), _("you have sent an invitation"), default=1)
+        notification.create_notice_type("friends_accept", _("Acceptance Received"), _("an invitation you sent has been accepted"), default=2)
+        notification.create_notice_type("friends_accept_sent", _("Acceptance Sent"), _("you have accepted an invitation you received"), default=1)
+        notification.create_notice_type("friends_otherconnect", _("Other Connection"), _("one of your friends has a new connection"), default=2)
+        notification.create_notice_type("join_accept", _("Join Invitation Accepted"), _("an invitation you sent to join this site has been accepted"), default=2)
     
     dispatcher.connect(create_notice_types, signal=signals.post_syncdb, sender=notification)
 except ImportError:
